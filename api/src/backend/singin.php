0dcea4010a0cfc3a08598bbac66d9d100faf0ef0
Camilo Yampuezan <142920365+camiloyampuezan@users.noreply.github.com>
2024-10-30 21:10:15 -0500

    Postman

--- api/src/backend/singin.php
+++ api/src/backend/singin.php
@@ -1,35 +1,27 @@
 <?php
+   //DB CONECTION
+   require('../../config/db_connection.php');
 
-    require('../../config/db_connection.php');
-    
-    $email=$_POST['email'];
-    $pass=$_POST['passwd'];
-    $enc_pass = md5($pass);
 
-    //validate if email already exists
-    $query = "SELECT * FROM users WHERE email='$email'";
+   //GET DATA FROM FORM SUBMISSION 
+
+   $email =$_POST['email'];
+   $pass = $_POST['passwd'];
+
+   $enc_pass = md5($pass);
+
+   //QUERY
+   $query = "select * from users where email='$email' and password='$enc_pass'";
     $result = pg_query($conn, $query);
     $row = pg_fetch_assoc($result);
 
-    if(row) {
-        echo "<script>alert('Email already exists!')</script>";
-        header('Refresh:0; url="http://127.0.0.1/beta/api/src/register_form.html');
-        exit();
-    }
+    if($row){
+        header('refresh:0; url=http://127.0.0.1/beta/api/src/home.php');
 
-    $query = "  INSERT INTO users (email, password)
-        VALUES ('$email', '$enc_pass')
-    ";
-
-    $result = pg_query($conn, $query);
-
-    if($result) {
-        //echo "Registration Successful!";
-        echo "<script>alert('Registration Successful!')</script>";
-        header('Refresh:0; url="http://127.0.0.1/beta/api/src/loging_form.html');
     } else {
-        echo "Registration fieled!: ";
+        echo "<script>alert('invalid email or password!')</script>";
+        header('refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
     }
-    pg_close($conn);
+
 
 ?>
